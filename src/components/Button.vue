741052f3d0648abd13d3a9996439d7ac7cbc6324
smyaseen <smyaseen164@gmail.com>
2021-11-09 10:49:41 +0500

    initial commit

---
+++ src/components/Button.vue
@@ -0,0 +1,16 @@
+<template>
+  <button @click="$emit('onClick')" :style="{ background: color }" class="btn">
+    {{ text }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: "Button",
+  props: {
+    text: String,
+    color: String,
+  },
+  emits: ["onClick"],
+};
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,36 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+    <Button
+      @onClick="$emit('toggle-show-add-task')"
+      :text="buttonText"
+      :color="showAddTask ? 'red' : 'green'"
+    />
+  </header>
+</template>
+
+<script>
+import Button from "./Button";
+
+export default {
+  name: "Header",
+  props: {
+    title: String,
+    buttonText: String,
+    showAddTask: Boolean,
+  },
+  components: {
+    Button,
+  },
+  emits: ["toggle-show-add-task"],
+};
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
